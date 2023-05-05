6f48a8fc450c3918f669331db9447b21d2494658
rmPablo1 <pablorubiiimonzo@gmail.com>
2023-05-05 12:18:55 +0200

    Updates

--- src/store/thunks/fetchMovie.js
+++
@@ -1,9 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios"
-const apiKey = "8fa81461"
-const fetchMovie = createAsyncThunk("singleMovie/fetch", async (movie) => {
-  const response = await axios.get(`https://www.omdbapi.com/?t=${movie}&apikey=${apiKey}`)
-  return response.data
-})
-
-export {fetchMovie}
